67268a48e93c74401b6fbd1f64f34847257cc4a5
Alex Rodriguez <alexrodart03@gmail.com>
2023-05-09 00:21:39 +0200

    Add validations and favicon

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,31 @@ import { ref } from 'vue';
 import data from './json/estacions.json';
 
 const serial = ref('');
-const linea = ref('???');
+const linea = ref('??');
 const estacio = ref('???');
-const zona = ref('???');
-const dia = ref('???');
-const mes = ref('???');
-const hora = ref('???');
-const minut = ref('???');
-const viatges = ref('???');
+const zona = ref('??');
+const diaMes = ref('??/??');
+const horaMin = ref('??:??');
+const viatges = ref('??');
+const vLinea = ref(null);
+const vEstacio = ref(null);
+const vZona = ref(null);
+const vDiaMes = ref(null);
+const vHoraMin = ref(null);
+const vViatges = ref(null);
 
 function checkSerial() {
-  let codi = serial.value.replace(/\s/g, '');
-  if (codi.length === 16) {
-    linea.value = checkLinea(codi.substring(0, 2));
-    estacio.value = checkEstacio(codi.substring(1, 4));
-    zona.value = codi.substring(4, 6);
-    dia.value = checkDia(codi.substring(6, 8), codi.substring(8, 10));
-    mes.value = checkMes(codi.substring(8, 10));
-    hora.value = codi.substring(10, 12);
-    minut.value = codi.substring(12, 14);
-    viatges.value = codi.substring(14, 16);
-  }
+  const codi = serial.value.replace(/\s/g, '');
+  linea.value = checkLinea(codi.substring(0, 2));
+  estacio.value = checkEstacio(codi.substring(1, 4));
+  zona.value = checkZona(codi.substring(4, 6));
+  diaMes.value = checkDiaMes(codi.substring(6, 8), codi.substring(8, 10));
+  horaMin.value = checkHoraMin(codi.substring(10, 12), codi.substring(12, 14));
+  viatges.value = checkViatges(codi.substring(14, 16));
 }
 
 function checkLinea(lineaMetro) {
+  vLinea.value = false;
   switch (lineaMetro) {
     case '01':
       return 'L1';
@@ -41,29 +42,69 @@ function checkLinea(lineaMetro) {
     case '09':
       return 'L9/L10';
     default:
-      return '???';
+      vLinea.value = true;
+      return '??';
   }
 }
 
 function checkEstacio(EstacioMetro) {
   const findEstacio = data.estacions.find(e => e.codi === EstacioMetro);
-  if (!findEstacio) return '???';
-
+  if (!findEstacio) {
+    vEstacio.value = true;
+    return '???';
+  }
+  vEstacio.value = false;
   return findEstacio.nom;
 }
 
-function checkDia(dia, mes){
-  const date = new Date(null, mes - 1, dia);
-  if (date.getMonth() + 1 != mes && date.getDate() != dia) {
-    return '???';
+function checkZona(zona) {
+  if (zona != '01') {
+    vZona.value = true;
+    return '??';
   }
-
-  return dia;
+  vZona.value = false;
+  return zona;
 }
 
-function checkMes(mes){
-  if (mes > 12) return '???';
-  return mes;
+function checkDiaMes(dia, mes){
+  const date = new Date(null, mes - 1, dia);
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  if (month == mes && day == dia) {
+    day = dia;
+    month = mes;
+    vDiaMes.value = false;
+    return `${day}/${month}`;
+  }
+
+  vDiaMes.value = true;
+  return `??/??`;
+}
+
+function checkHoraMin(hora, minut) {
+ let hour = hora;
+ let minute = minut;
+ vHoraMin.value = false;
+ if (hora > 24) {
+  hour = '??';
+  vHoraMin.value = true;
+ }
+
+ if (minut > 59) {
+  minute = '??';
+  vHoraMin.value = true;
+ }
+ 
+ return `${hour}:${minute}`;
+}
+
+function checkViatges(viatges) {
+  if (viatges > 9) {
+    vViatges.value = true;
+    return '??';
+  }
+  vViatges.value = false;
+  return viatges;
 }
 
 </script>
@@ -77,45 +118,35 @@ function checkMes(mes){
       </label>
     </form>
     <div class="parent">
-      <div class="div1">
-        <label for="linea" class="">Linea:
-          <input type="text" id="linea" disabled v-model="linea">
+      <div class="a">
+        <label for="linea">Linea:
+          <input type="text" id="linea" readonly v-model="linea" :aria-invalid="vLinea">
         </label>
       </div>
-      <div class="div2">
-        <label for="estacio" class="">Estació:
-          <input type="text" id="estacio" disabled v-model="estacio" >
+      <div class="b">
+        <label for="estacio">Estació:
+          <input type="text" id="estacio" readonly v-model="estacio" :aria-invalid="vEstacio">
         </label>
       </div>
-      <div class="div3">
+      <div class="c">
         <label for="zona">Zona:
-          <input type="text" id="zona" disabled v-model="zona">
+          <input type="text" id="zona" readonly v-model="zona" :aria-invalid="vZona">
         </label>
       </div>
 
-      <div class="div4">
-        <label for="dia">Dia:
-          <input type="text" id="dia" disabled v-model="dia">
+      <div class="d">
+        <label for="dia">Dia i Mes:
+          <input type="text" id="dia" readonly v-model="diaMes" :aria-invalid="vDiaMes">
         </label>
       </div>
-      <div class="div5">
-        <label for="mes">Mes:
-          <input type="text" id="mes" disabled v-model="mes">
+      <div class="e">
+        <label for="hora">Hora i minut:
+          <input type="text" id="hora" readonly v-model="horaMin" :aria-invalid="vHoraMin">
         </label>
       </div>
-      <div class="div6">
-        <label for="hora">Hora:
-          <input type="text" id="hora" disabled v-model="hora">
-        </label>
-      </div>
-      <div class="div7">
-        <label for="minut">Minut:
-          <input type="text" id="minut" disabled v-model="minut">
-        </label>
-      </div>
-      <div class="div8">
+      <div class="f">
         <label for="viatges">Viatges:
-          <input type="text" id="viatges" disabled v-model="viatges">
+          <input type="text" id="viatges" readonly v-model="viatges" :aria-invalid="vViatges">
         </label>
       </div>
     </div>
@@ -139,21 +170,23 @@ function checkMes(mes){
   opacity: 0.69;
   text-align: center;
 }
+
 .parent {
-display: grid;
-grid-template-columns: repeat(12, 1fr);
-grid-template-rows: repeat(3, 1fr);
-grid-column-gap: 12px;
+  display: grid; 
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr; 
+  grid-template-rows: 1fr 1fr; 
+  gap: 0px 15px; 
+  grid-template-areas: 
+    "a a b b b b c"
+    "d d d e e e f"; 
 max-width: 800px;
 margin-top: 100px;
 }
 
-.div1 { grid-area: 1 / 1 / 2 / 4; }
-.div2 { grid-area: 1 / 4 / 2 / 10; }
-.div3 { grid-area: 1 / 10 / 2 / 13; }
-.div4 { grid-area: 2 / 1 / 3 / 4; }
-.div5 { grid-area: 2 / 4 / 3 / 7; }
-.div6 { grid-area: 2 / 7 / 3 / 10; }
-.div7 { grid-area: 2 / 10 / 3 / 13; }
-.div8 { grid-area: 3 / 5 / 4 / 9; }
+.a { grid-area: a; }
+.b { grid-area: b; }
+.c { grid-area: c; }
+.d { grid-area: d; }
+.e { grid-area: e; }
+.f { grid-area: f; }
 </style>
